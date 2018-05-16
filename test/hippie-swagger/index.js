67135154e4b1245a2d0c0c9d9c5ac90e90243292
Travis Crowe <travis.crowe@tasconline.com>
2018-05-16 09:59:00 -0500

    feat(hippie-swagger): finished hippie-swagger implementation and cleaned up script

--- test/hippie-swagger/index.js
+++ test/hippie-swagger/index.js
@@ -6,52 +6,83 @@ var hippie = require('hippie');
 var app = require('../../app');
 var expect = require('chai').expect;
 var path = require('path');
-var dereferencedSwagger;
+var swagger;
 
-describe('/cars/{id}', function () {
-  this.timeout(10000);
-
+describe('api', function() {
     before(function (done) {
         parser.dereference(path.join(__dirname, '../../api/swagger/swagger.yaml'), function (err, api) {
             if (err) return done(err);
-            dereferencedSwagger = api;
+            swagger = api;
             done();
-        })
-    })
-
-    describe('get', function () {
-        it('should respond with 200', function (done) {
-            hippie(app, dereferencedSwagger)
-            .get('/cars/{id}')
-            .pathParams({
-                id: 'bc658950-57b8-11e8-9cfa-aded4da2e135'
-            })
-            .expectStatus(200)
-            .end(done)
         });
     });
 
-    describe('put', function() {
-        it('should respond with 200', function(done) {
-            hippie(app. dereferencedSwagger)
-                .put('/cars/{id}')
-                .pathParams({
-                    id: 'bc658950-57b8-11e8-9cfa-aded4da2e135'
-                })
-                .send({"year":"2017","make":"Mazda","model":"CX-5","color":{"primary":"#F00","secondary":"#B4R"},"mileage":7478.081694229355})
-                .expectStatus(200)
-                .end(done)
+    describe('/cars/{id}', function () {
+        describe('get', function () {
+            it('should respond with 200', function (done) {
+                hippie(app, swagger)
+                    .get('/cars/bc658950-57b8-11e8-9cfa-aded4da2e135')
+                    .expectStatus(200)
+                    .end(done);
+            });
+
+            it('should respond with 404', function(done) {
+                hippie(app, swagger)
+                    .get('/cars/fakeId')
+                    .expectStatus(404)
+                    .end(done)
+            });
+        });
+
+        describe('put', function() {
+            it('should respond with 200', function(done) {
+                hippie(app, swagger)
+                    .json()
+                    .put('/cars/bc658950-57b8-11e8-9cfa-aded4da2e135')
+                    .send({"year":"2017","make":"Mazda","model":"CX-5","color":{"primary":"#F00","secondary":"#B4R"},"mileage":7478.081694229355})
+                    .expectStatus(200)
+                    .end(done);
+            });
+
+            it('should respond with 404', function(done) {
+                hippie(app, swagger)
+                    .json()
+                    .put('/cars/fakeId')
+                    .send({"year":"2017","make":"Mazda","model":"CX-5","color":{"primary":"#F00","secondary":"#B4R"},"mileage":7478.081694229355})
+                    .expectStatus(404)
+                    .end(done)
+            });
+        });
+
+        describe('delete', function() {
+            it('should respond with 404', function(done) {
+                hippie(app, swagger)
+                    .del('/cars/fakeId')
+                    .expectStatus(404)
+                    .end(done);
+            });
         });
     });
-});
 
-describe('/cars', function() {
-    describe('get', function() {
-        it('should respond with 200', function(done) {
-            hippie(app, dereferencedSwagger)
-                .get('/cars')
-                .expectStatus(200)
-                .end(done)
+    describe('/cars', function() {
+        describe('get', function() {
+            it('should respond with 200', function(done) {
+                hippie(app, swagger)
+                    .get('/cars')
+                    .expectStatus(200)
+                    .end(done)
+            });
+        });
+
+        describe('post', function() {
+            it('should respond with 200', function(done) {
+                hippie(app, swagger)
+                    .json()
+                    .post('/cars')
+                    .send({"year":"2017","make":"Mazda","model":"CX-5","color":{"primary":"#F00","secondary":"#B4R"},"mileage":7478.081694229355})
+                    .expectStatus(200)
+                    .end(done);
+            });
         });
     });
 });
